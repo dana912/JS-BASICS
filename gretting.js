756ae3d82c6defbd07e105ba8cfbb51c2c0dc5da
dana912 <danadev912@gmail.com>
2021-01-19 13:08:43 +0900

    clock & username

--- gretting.js
+++ gretting.js
@@ -1,11 +1,27 @@
-const form = document.querySelector(".js-form");
-input = form.querySelector("input");
-greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
   SHOWINH_CN = "showing";
 
-function paintGreetind(text) {
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currnetValue = input.value;
+  paintGreeting(currnetValue);
+  saveName(currnetValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWINH_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
   form.classList.remove(SHOWINH_CN);
   greeting.classList.add(SHOWINH_CN);
   greeting.innerText = `Hello ${text}`;
@@ -14,8 +30,9 @@ function paintGreetind(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
+    askForName();
   } else {
-    paintGreetind(currentUser);
+    paintGreeting(currentUser);
   }
 }
 
